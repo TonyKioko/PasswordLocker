53b00802c91425ed0153ed7938059a4f6397643e
TonyKioko <tonykioko384@gmail.com>
2018-08-23 22:27:19 +0300

    multiple credentials save test pass

--- users_credentials_test.py
+++ users_credentials_test.py
@@ -19,13 +19,20 @@ class TestUser(unittest.TestCase):
 class TestCredential(unittest.TestCase):  
 
     def setUp(self):
-        self.new_credential = Credential("Slack","xyz")   
+        self.new_credential = Credential("Slack","xyz456")   
     def test_credentials(self):
         self.assertEqual(self.new_credential.account_name,"Slack")  
-        self.assertEqual(self.new_credential.account_password,"xyz")
+        self.assertEqual(self.new_credential.account_password,"xyz456")
     def test_save_credentials(self):
         self.new_credential.save_credentials() 
         self.assertEqual(len(Credential.credentials_list), 1) 
+    def test_save_multiple_credentials(self):
+        self.new_credential.save_credentials()
+        test_credential = Credential("eCitizen","jkl789")
+        test_credential.save_credentials()
+        self.assertEqual(len(Credential.credentials_list), 2)
+    def tearDown(self):
+        Credential.credentials_list = []
 
 if __name__ == '__main__':
     unittest.main()
