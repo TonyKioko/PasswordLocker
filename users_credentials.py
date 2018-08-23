8669af0e849cfd1a9e314fd813107e8ba785df70
TonyKioko <tonykioko384@gmail.com>
2018-08-23 22:10:59 +0300

    credentials setup test pass

--- users_credentials.py
+++ users_credentials.py
@@ -1,7 +1,7 @@
 
 class User:
     # pass
-    members_list = []
+    user_list = []
     def __init__(self,first_name,last_name,password):
         self.first_name = first_name
         self.last_name = last_name
--- users_credentials_test.py
+++ users_credentials_test.py
@@ -1,5 +1,6 @@
 import unittest
 from users_credentials import User
+from users_credentials import Credential
 
 class TestUser(unittest.TestCase):
     def setUp(self):
@@ -8,11 +9,20 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.first_name,'Tony')
         self.assertEqual(self.new_user.last_name,'Kioko')
         self.assertEqual(self.new_user.password,'abc123')
-        
+
     def test_save_user(self):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
-        
+
+
+
+class TestCredential(unittest.TestCase):  
+
+    def setUp(self):
+        self.new_credential = Credential("Slack","xyz")   
+    def test_credentials(self):
+        self.assertEqual(self.new_credential.account_name,"Slack")  
+        self.assertEqual(self.new_credential.account_password,"xyz")  
 
 if __name__ == '__main__':
     unittest.main()
