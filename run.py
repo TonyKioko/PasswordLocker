13764092592f5682ffdc644d568a763f9c90e55a
TonyKioko <tonykioko384@gmail.com>
2018-08-24 20:42:59 +0300

    authenticate user function success

--- run.py
+++ run.py
@@ -2,12 +2,15 @@
 from users_credentials import User
 from users_credentials import Credential
 
-def create_user(self,fname,lname,password):
+def create_user(fname,lname,password):
     new_user = User(fname,lname,password)
     return new_user
 
 def save_user(user):
-    user.save_user()
+    User.save_user(user)
+def authenticate_user(fname):
+    confirm_user = User.user_exists(fname)
+    return confirm_user
 
 def make_password(self):
     password = Credential.automate_password()
@@ -36,8 +39,25 @@ def main():
 
     while True:
         print("\n")
-        print("Use these short codes to select an option: CA - to create account , LG - to log in, fc -find a contact, ex - to exit password Locker ")
+        print("Use these short codes to select an option: ca - to create account , li - to log in, ex - to exit password Locker ")
         short_code = input().lower()
+        print('\n')
+
+        if short_code == 'ca':
+            print("Create an Account")
+            first_name = input('Enter your first name - ').strip()
+            last_name = input('Enter your last name - ').strip()
+            password = input('Enter your password - ').strip()
+            save_user(create_user(first_name, last_name, password))
+            print(f'New Account Created for: {first_name} {last_name} using password: {password}')
+
+        elif short_code == "li":
+            print("*"*40)
+            print("Enter details to access your account")
+            user_name = input("Enter first name: ").strip()
+            password = str(input("Enter password: "))
+            user_exists = authenticate_user(user_name)
+
     else:
           print("I really didn't get that. Please use the short codes")
 
