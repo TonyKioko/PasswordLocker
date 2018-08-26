e39da9b9f12dbaf2efbb7ab42d0d7a17e5de5e7a
TonyKioko <tonykioko384@gmail.com>
2018-08-26 11:46:00 +0300

    identation errors on docstrings fixed

--- password_manager.py
+++ password_manager.py
@@ -4,18 +4,31 @@ import string
 import pyperclip
 
 class User:
+    """
+    Class that generates new instances of users
+    """
     # pass
-    list_of_users = []
+    list_of_users = [] # Empty list of users
+
     def __init__(self,first_name,email,password):
+
+        """ the init method helps in defining properties of our objects"""
         self.first_name = first_name
         self.email = email
         self.password = password
 
     def save_user(self):
+        '''
+        save_user method saves contact objects into list_of_users
+        '''
+
         User.list_of_users.append(self)
 
     @classmethod
     def user_exists(cls,first_name):
+        '''
+        Method that checks if a user exists from the list_of_users
+        '''
         for user in cls.list_of_users:
             if (user.first_name == first_name):
                 return True
@@ -28,51 +41,62 @@ class User:
         return False
 
 class Credential:
+    """This is a class for creating new instances, methods and properties of credentials"""
 
-    list_of_credentials = []
-    # @classmethod
-    # def user_exists(cls,first_name):
-    #     for user in cls.user_list:
-    #         if user.first_name == first_name:
-    #             return True
-    #     return False
-    # @classmethod
-	# def authenticate_user(cls,first_name,password):
-	# 	current_user = ''
-	# 	for user in cls.user_list:
-	# 		if (user.first_name == first_name and user.password == password):
-	# 			current_user = user.first_name
-	# 	return current_user
+    list_of_credentials = [] #Empty list of credentials
+   
     def __init__(self,account_name,user_name,account_password):
+
+        '''
+        init method helps us define properties for our objects
+        '''
         self.account_name = account_name
         self.user_name = user_name
         self.account_password = account_password
     def save_credentials(self):
+        '''
+        Method to save each new credential to the list of credetials
+        '''
         Credential.list_of_credentials.append(self)
 
     def automate_password(chars=string.ascii_letters + string.digits):
+        """
+        Method returns a string of random characters. Length of password to be determined by user.
+        
+        Source: http://stackoverflow.com/a/2257449
+        """
 
         length = int(input("Enter length of password: "))
         auto_password = ''.join(random.choice(chars) for i in range(length))
         return auto_password
     def delete_credentials(self):
+        """Method deletes credentials from list of credentials"""
+
         Credential.list_of_credentials.remove(self)
     @classmethod
     def find_by_website(cls,account_name):
+        """Method that returns saved credentials for a specific website"""
         for credential in cls.list_of_credentials:
             if credential.account_name == account_name:
                 return credential
     @classmethod
     def credential_exists(cls,account_name):
+        """Method that checks if credentials exist from the list of credentials 
+        Returns :
+            Boolean: True or false depending if the credentials exists
+        """
         for credential in cls.list_of_credentials:
             if credential.account_name == account_name:
                 return True
         return False
     @classmethod
     def display_all_credentials(cls):
+
+        """Method displays all saved credentials for a given user"""
         return cls.list_of_credentials
     @classmethod
     def copy_password(cls,account_name):
+        """Method that enables user to copy password for specific website"""
         credential_found = Credential.find_by_website(account_name)
         return pyperclip.copy(credential_found.account_password)
         print(pyperclip.copy(credential_found.account_password))
--- password_manager_test.py
+++ password_manager_test.py
@@ -1,20 +1,36 @@
-import unittest
+import unittest # Importing the unittest module
 import pyperclip
-from password_manager import User
-from password_manager import Credential
+from password_manager import User  # Importing the User class
+from password_manager import Credential  # Importing the Credential class
 
 class TestUser(unittest.TestCase):
+
+    
     def setUp(self):
-        self.new_user = User('Tony','tony@abc.com','abc123')
+        '''
+        Setup method to run before each test cases.
+        '''
+        self.new_user = User('Tony','tony@abc.com','abc123') ## create user object
     def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
         self.assertEqual(self.new_user.first_name,'Tony')
         self.assertEqual(self.new_user.email,'tony@abc.com')
         self.assertEqual(self.new_user.password,'abc123')
 
     def test_save_user(self):
-        self.new_user.save_user()
+        '''
+        test_save_user test case to test if the user object is saved into
+        the list of users
+        '''
+        self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.list_of_users),1)
     def test_user_exists(self):
+        '''
+        test_user_exists test case to test if the user exists in the
+        list of users.
+        '''
         self.new_user = User("Tony","tony@abc.com","abc123")
         self.new_user.save_user()
         user_exists =  User.user_exists("Tony")
@@ -23,39 +39,50 @@ class TestUser(unittest.TestCase):
 
 
 class TestCredential(unittest.TestCase):
+    '''
+    Test class that defines test cases for the credential class behaviours.
 
-
-    # def test_authenticate_user(self):
-
-    #     self.new_user = User('Tony','Kioko','abc123')
-    #     self.new_user.save_user()
-    #     next_user = User('Tommy','Kioko','abc123')
-    #     next_user.save_user()
-
-    #     for user in User.usersxyz456
-    #         if user.first_name == next_user.first_name and user.password == next_user.password:
-    #             current_user = user.first_name
-    #     return current_user
-
-    # self.assertEqual(current_user,Credential.authenticate_user(next_user.password,next_user.first_name))
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
 
     def setUp(self):
+        '''
+        Setup method to run before each test cases.
+        '''
         self.new_credential = Credential("Slack","Tonykm","xyz456")
-    def test_credentials(self):
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
         self.assertEqual(self.new_credential.account_name,"Slack")
         self.assertEqual(self.new_credential.user_name,"Tonykm")
         self.assertEqual(self.new_credential.account_password,"xyz456")
     def test_save_credentials(self):
+        '''
+        test_save_credential test case to test if the credential object is saved into
+        the list of credentials
+        '''
         self.new_credential.save_credentials()
         self.assertEqual(len(Credential.list_of_credentials), 1)
     def test_save_multiple_credentials(self):
+        '''
+        test_save_multiple_credentials to check if we can save multiple credential
+        objects to our list_of_credentials
+        '''
         self.new_credential.save_credentials()
         test_credential = Credential("Slack","Tonykm","xyz456")
         test_credential.save_credentials()
         self.assertEqual(len(Credential.list_of_credentials), 2)
     def tearDown(self):
+        '''
+        tearDown method that cleans up the list_of_credentials after each test case has run.
+        '''
         Credential.list_of_credentials = []
     def test_delete_credentials(self):
+        '''
+        test_delete_credentials to test if we can remove a credential from our list_of_credentials
+        '''
         self.new_credential.save_credentials()
         test_credential = Credential("Slack","Tonykm","xyz456")
         test_credential.save_credentials()
